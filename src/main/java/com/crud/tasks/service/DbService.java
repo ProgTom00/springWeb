776e021abdea3747664fcd2248b3686c67e300b9
Tomasz Olechowski <tonaszolechowski1992@gmail.com>
2022-03-08 16:49:24 +0100

    Jar and PostgreSQL fixed

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -2,7 +2,6 @@ package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +9,11 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
 public class DbService {
 
-    @Autowired
-    private final TaskRepository repository;
+        @Autowired
+    private TaskRepository repository;
+
 
     public List<Task> getAllTasks() {
         return repository.findAll();
