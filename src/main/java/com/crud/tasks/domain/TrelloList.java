5e29dc203aa51ec195e315a6fcaeec2e5614e815
Tomasz Olechowski <tonaszolechowski1992@gmail.com>
2022-03-19 14:32:41 +0100

    update v2

--- src/main/java/com/crud/tasks/domain/TrelloList.java
+++ src/main/java/com/crud/tasks/domain/TrelloList.java
@@ -1,12 +1,10 @@
 package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import org.springframework.stereotype.Component;
+import lombok.Getter;
 
-@Component
+@Getter
 @AllArgsConstructor
-@Data
 public class TrelloList {
     private String id;
     private String name;
